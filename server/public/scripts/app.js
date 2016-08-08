4f43c05aeb5f595b5f50ee4834c5c0e972bbf8b2
Adam East <adam.eastvold@gmail.com>
2016-08-08 09:01:27 -0500

    added some comments and attempted timer

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,109 +1,134 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  function buildNavSelectors(omicron){
-    for(var i = 0; i < 17; i++) {
-    $('.selectors').append('<div id="student' + i + '" class="selector"></div>');
-    updateNavBar(0);
-  };
-  };
-var timerStart = false;
+    function buildNavSelectors(omicron) {
+        for (var i = 0; i < 17; i++) {
+            $('.selectors').append('<div id="student' + i + '" class="selector"></div>');
+            updateNavBar(0);
+        };
+    };
 
-  buildNavSelectors();
+    var timerStart = false;
 
-loadData();
+    //  Append selector buttons to DOM
 
-function loadData(){
+    buildNavSelectors();
 
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      success: function(data){
-        appendDOM(data.omicron);
-        timerNav();
+    // The AJAX call, also includes all appending to DOM and my Timer start
+
+    loadData();
+
+    function loadData() {
+
+        $.ajax({
+            type: "GET",
+            url: "/data",
+            success: function(data) {
+                appendDOM(data.omicron);
+                timerNav();
 
 
-      }
+            }
 
+        });
+
+    };
+
+    // Declared global as a counter
+
+    var currentStudent = 0;
+
+    // nextStudent and prevStudent are my buttons which use the global counter
+
+    function nextStudent() {
+        updateNavBar(currentStudent);
+        currentStudent++;
+        $('.display').empty();
+        if (currentStudent == 17) {
+            currentStudent = 0;
+        };
+        loadData();
+        updateNavBar(currentStudent);
+    };
+
+    $('.next').on('click', function() {
+        nextStudent(); //create one listener for both buttons with cases in a switch statement
     });
 
-  };
+    function prevStudent() {
+        updateNavBar(currentStudent);
+        currentStudent--;
+        $('.display').empty();
+        if (currentStudent == -1) {
+            currentStudent = 16;
+        };
+        loadData();
+        updateNavBar(currentStudent);
+        timerNav();
+    };
+
+    $('.prev').on('click', function() {
+        prevStudent();
+    });
+
+    // This combines all of my appending to DOM functions
+
+    function appendDOM(omicron) {
+        var studentList = omicron;
+        loadName(studentList);
+        loadGitHub(studentList);
+        loadShoutOut(studentList);
+    };
+
+    // This appends the names of students
+    function loadName(omicron) {
+        var studentList = omicron;
+        $('.display').append('<h1 class="personName">' + studentList[currentStudent].name + '</h1>');
+    };
+
+    // This appends the GitHub Username
+
+    function loadGitHub(omicron) {
+        var studentList = omicron;
+        $('.display').append('<h3 class="gitUsername">Git Username:  ' + studentList[currentStudent].git_username + '</h3>');
+    };
+
+    // This appends the shoutouts
+
+    function loadShoutOut(omicron) {
+        var studentList = omicron;
+        $('.display').append('<h3 class="shoutout">Shoutout:  ' + studentList[currentStudent].shoutout + '</h3>');
+    };
 
 
+    // I used the following to test things
+    // $('body').on("click", "div", function() {
+    //     console.log("data", $(this).data("studentnum")); //USE THIS ALL THE TIME
+    //     //change global student count
+    //     //appendDom()
+    // });
 
-var currentStudent = 0;
+    // The following function will toggleClass for the color on the selectors
 
-function nextStudent(){
-  updateNavBar(currentStudent);
-  currentStudent++;
-  $('.display').empty();
-  if(currentStudent == 17){
-    currentStudent = 0;
-  };
-  loadData();
-  updateNavBar(currentStudent);
-};
+    function updateNavBar(index) {
+        $('#student' + index).toggleClass('selected');
 
-$('.next').on('click', function(){
-  nextStudent();    //create one listener for both buttons with cases in a switch statement
-});
+    };
 
-function prevStudent(){
-  updateNavBar(currentStudent);
-  currentStudent--;
-  $('.display').empty();
-  if(currentStudent == -1){
-    currentStudent = 16;
-  };
-  loadData();
-  updateNavBar(currentStudent);
-};
+    // timerNav is how I created the timer
 
-$('.prev').on('click', function(){
-  prevStudent();
-});
+    function timerNav() {
 
-function appendDOM(omicron){
-  var studentList = omicron;
-  loadName(studentList);
-  loadGitHub(studentList);
-  loadShoutOut(studentList);
-};
+        if (timerStart == false) {
+            setInterval(nextStudent, 5000);
+            timerStart = true;
+        };
 
-function loadName(omicron){
-  var studentList = omicron;
-    $('.display').append('<h1 class="personName">' + studentList[currentStudent].name + '</h1>');
-};
-function loadGitHub(omicron){
-  var studentList = omicron;
-    $('.display').append('<h3 class="gitUsername">Git Username:  ' + studentList[currentStudent].git_username + '</h3>');
-};
-function loadShoutOut(omicron){
-  var studentList = omicron;
-    $('.display').append('<h3 class="shoutout">Shoutout:  ' + studentList[currentStudent].shoutout + '</h3>');
-};
+    };
 
 
-$('body').on("click","div",function(){
-  console.log("data",$(this).data("studentnum"));    //USE THIS ALL THE TIME
-  //change global student count
-  //appendDom()
-});
+// Made attempt at the timer reset but it definitely did not work :/
 
-function updateNavBar(index){
-  $('#student' + index).toggleClass('selected');
-
-
-  //acces currentstudent
-  //toggle class (to change color)
-};
-
-function timerNav(){
-
-  if(timerStart == false){
-    setInterval(nextStudent, 10000);
-    timerStart = true;
-  };
-
-};
-
+    // function timerReset(){
+    //   clearInterval(timer, 5000);
+    // }
 });
